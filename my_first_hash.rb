86c270629e9698d0c754bdc9f0df4fe94d39908a
Andrew Waller <andrewhwaller@gmail.com>
2019-03-18 17:31:54 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,7 +9,7 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information:
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
-  shipping_manifest = {
+  the_manifest = {
             "whale bone corsets" => "5",
             "porcelain vases" => "2",
             "oil paintings" => "3"
@@ -43,5 +43,5 @@ def adding
 
 
   # return the shipping_manifest hash below
-  return shipping_manifest
+  shipping_manifest
 end
